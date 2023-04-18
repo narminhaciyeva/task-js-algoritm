3630586dd255459ffa0f2d9897af554a84900e23
Narmin <127186131+narminhaciyeva@users.noreply.github.com>
2023-04-18 13:58:48 +0400

    Add files via upload

---
+++ js-praktika 19.04/assets/js/main.js
@@ -0,0 +1,70 @@
+// // Cevrenin sahesini tapan proqram tertib edin
+
+
+// function areaOfCircle(radius){
+//     return 3*radius*radius
+// }
+// console.log(areaOfCircle(3));
+
+
+
+
+// // Duzbucaqlinin sahesini tapan proqram tertib edin
+// var a=7;
+// var b=4;
+
+// function areaOfRectangular(sahe){
+   
+//     var calc=(a,b);
+//     return  a*b
+// }
+// console.log(areaOfRectangular(a,b));
+
+
+
+
+// Duzbucaqli paralelepipedin tam sethinin sahesi
+
+// var a=4;
+// var b=5;
+// var c=6;
+
+
+// function areaOfTheFullSet(sahe){
+//     var calc=(a,b,c);
+//     return 2*(a*b+a*c+b*c)
+// }
+// console.log(areaOfTheFullSet(a,b,c));
+
+
+
+
+
+// Ucbucagin daxilline cekilmis cevrenin sahesi
+// var a=5;
+// var b=5;
+// var c=2;
+// var r=5;
+// function areaOfATriangle(sahe){
+//     var calc=(a,b,c);
+//     return (a+b+c)/2*r
+// }
+// console.log(areaOfATriangle(a,b,c));
+
+
+// Proqram ikirəqəmli ədədi oxuyur və ədədin hər bir rəqəmini arada boşluq olmaqla ekrana verir.
+
+// function number(x){
+//     if(x>10 && x<99)
+//     var a=x%10
+//     var b=(x-a)/10
+//     return b+ " " +a
+// }
+// console.log(number(52));
+  
+
+
+// Verilmis string deyerinde verilmis char-in hansi index-de yerlesdiyini tapib qaytaran function
+
+// .Misal ucun metodu call edib "salam" ve 'L deyerini' gondersek gerite 2 qaytarmalidir amma har hemin string-de olmasa -1 qaytarmalidir.
+
